426d5a25ab47f77260e84c69f72068a720ebdcf7
Michal <michalplebanski@gmail.com>
2019-12-08 09:19:38 +0100

    Updates

---
+++ precache-manifest.3974ff46194ef40d8cec1bf9c7db78c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb94f21e69c7dd1f8d1522432a880cac",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "ac23d5935d5548988535",
+    "url": "/react-calculator/static/css/main.160d3d42.chunk.css"
+  },
+  {
+    "revision": "87c0dce59687cab08176",
+    "url": "/react-calculator/static/js/2.f80dabe0.chunk.js"
+  },
+  {
+    "revision": "ac23d5935d5548988535",
+    "url": "/react-calculator/static/js/main.823fa338.chunk.js"
+  },
+  {
+    "revision": "204e8c8e9334f8f1a3cf",
+    "url": "/react-calculator/static/js/runtime-main.94349eff.js"
+  }
+]);
